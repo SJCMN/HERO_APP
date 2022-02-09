b6862c68ccfa87b0e4151387e1bbedfb74a40245
SJCMN <sam.clavette@gmail.com>
2022-02-09 11:30:17 -0600

    add buttons, css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent, HerosComponent],
+  declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Dr. Chump' },
+  { id: 2, name: 'Dr. Chimp' },
+  { id: 3, name: 'Dr. Strange' },
+  { id: 4, name: 'Dr. Bruce Banner' },
+  { id: 5, name: 'Mr. Tony Stark' },
+  { id: 6, name: 'Thor' },
+  { id: 7, name: 'Magneto' },
+  { id: 8, name: 'Spiderman' },
+  { id: 9, name: 'Mr. Norman Osborn' },
+  { id: 10, name: 'Thanos' },
+  { id: 11, name: 'Vision' },
+  { id: 12, name: 'Mr. Peter Quill' }
+];
