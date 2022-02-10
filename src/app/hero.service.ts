a85b65d976a117a27d7e7d3d8ec4a2e3fd111746
SJCMN <sam.clavette@gmail.com>
2022-02-10 12:35:19 -0600

    refactor angular router

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,15 +5,21 @@ import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 
   constructor(private messageService: MessageService) {}
